a7db91c1811c022117bbc212c8d6eaa037cd2546
John Kelly <johkelly@mines.edu>
2013-11-11 17:05:07 -0700

    Smooth target movement. Pause on touch. Repeated firing event with pause/resume.

--- src/com/example/ReverseShootingGallery/GameplayView.java
+++ src/com/example/ReverseShootingGallery/GameplayView.java
@@ -3,22 +3,25 @@ package com.example.ReverseShootingGallery;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Rect;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created with IntelliJ IDEA.
- * Based on http://www.mindfiresolutions.com/Using-Surface-View-for-Android-1659.php
+ * Based very loosely on:
+ * http://www.mindfiresolutions.com/Using-Surface-View-for-Android-1659.php
+ * http://www.codeproject.com/Articles/228656/Tilt-Ball-Walkthrough
  */
-public class GameplayView extends SurfaceView implements SensorEventListener, SurfaceHolder.Callback {
+public class GameplayView extends SurfaceView implements SensorEventListener, SurfaceHolder.Callback, View.OnTouchListener {
 
     private static final String logString = GameplayView.class.getName()+".log";
 
@@ -27,24 +30,38 @@ public class GameplayView extends SurfaceView implements SensorEventListener, Su
     private SensorManager sensorManager;
     private Sensor accelSensor;
 
-    private float[] lastGrav;
-    private float[] lastMag;
-
     private GameThread gameThread;
+    private Handler threadHandler;
+    private Runnable shotTimer;
+    private long lastShotResume;
+    private long shotWaitElapsed;
 
-    private Context context;
+    private final Context mContext;
+    private boolean paused = false;
 
     public GameplayView(Context context) {
         super(context);
         target = new DrawableTarget(300, 300, R.drawable.target_blue, getResources());
-        this.context = context;
+        this.mContext = context;
         SurfaceHolder holder = getHolder();
         holder.addCallback(this);
+        setOnTouchListener(this);
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        threadHandler = new Handler();
+        shotTimer = new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(mContext, "Shot fired!", Toast.LENGTH_SHORT).show();
+                lastShotResume = System.currentTimeMillis();
+                threadHandler.postDelayed(this, 5000);
+                shotWaitElapsed = 0;
+            }
+        };
+        threadHandler.postDelayed(shotTimer, 5000);
     }
 
     public void registerSensor(){
-        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
-        accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_GAME);
     }
 
@@ -62,14 +79,45 @@ public class GameplayView extends SurfaceView implements SensorEventListener, Su
     }
 
     public void manualDraw(Canvas c){
-        c.drawColor(Color.BLACK);
+        c.drawColor(Color.WHITE);
         target.clamp(getWidth(), getHeight());
         target.draw(c);
+        if(paused){
+            c.drawColor(Color.argb(128, 0,0,0));
+        }
+    }
+
+    public void gameplayPause(){
+        paused = true;
+        unregisterSensor();
+        target.setVelocity(0,0);
+        shotWaitElapsed += (System.currentTimeMillis() - lastShotResume);
+        threadHandler.removeCallbacks(shotTimer);
+    }
+
+    public void gameplayUnpause(){
+        paused = false;
+        registerSensor();
+        lastShotResume = System.currentTimeMillis();
+        threadHandler.postDelayed(shotTimer, 5000-shotWaitElapsed);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event){
+        if(v == this){
+            if(!paused){
+                gameplayPause();
+            } else {
+                gameplayUnpause();
+            }
+        }
+        return false;
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
         target.setVelocity(event.values[1], event.values[0]);
+        Log.d(logString, event.values[1] + " " + event.values[0]);
     }
 
     @Override
@@ -79,7 +127,7 @@ public class GameplayView extends SurfaceView implements SensorEventListener, Su
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        gameThread = new GameThread(holder, context, this);
+        gameThread = new GameThread(holder, mContext, this);
         gameThread.running = true;
         gameThread.start();
     }
